c424ffede8f4af6df16ca57867295aa67529012f
Christian Gärtner <christiangaertner.film@googlemail.com>
2013-05-20 13:35:49 +0200

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,48 @@ CodeIgniter-ACL-Hook
 ====================
 
 A simple to use ACL for CodeIgniter
+
+Introduction
+------------
+
+CodeIgniter is a great PHP-Framework with a lot of features.
+However it does not provide a ACL from ground up.
+
+ACL
+---
+
+An ACL, short for Access Control List, is simple and easy maintainable way of controlling permissions.
+
+This ACL is based on the post_controller_constructor hook of CodeIgniter.
+
+Installation
+------------
+
+* Download the hook acl.php
+* Place the file into application/hooks
+* Open the main config file and change $config['enable_hooks'] = FALSE; to $config['enable_hooks'] = TRUE;
+* Hook the acl class into the system by editing application/config/hooks.php
+		$hook['post_controller_constructor'] = array(
+    		'class' => 'ACL',
+    		'function' => 'auth',
+    		'filename' => 'acl.php',
+    		'filepath' => 'hooks'    
+		);
+
+* Now you just need to add your rules:
+	In the contstructor of the class:
+		$this->role_field = 'role_id';
+	This is the name of field in the session which indicates the role id of the user
+	
+	Now you can add rules like this:
+		$this->perms[<ROLE_ID>][<CONTROLLER>][<METHOD>] = true;
+		
+	The first array is setting the rule for the role id. The next two values are defining the controller and method.
+		$this->perms[2]['admin']['index'] = true;
+	This for example will allow everyone with the role_id 2 to access BASE_URL/admin(/index)
+	
+	
+TODO
+----
+
+A good way to control redirect.
---
+++ acl.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * This class will be called by the post_controller_constructor hook and act as ACL
+ * 
+ * @author ChristianGaertner
+ */
+class ACL {
+    
+    /**
+     * Array to hold the rules
+     * Keys are the role_id and values arrays
+     * In this second level arrays the key is the controller and value an array with key method and value boolean
+     * @var Array 
+     */
+    private static $perms;
+    /**
+     * The field name, which holds the role_id
+     * @var string 
+     */
+    private static $role_field;
+    /**
+     * Contstruct in order to set rules
+     * @author ChristianGaertner
+     */
+    public function __construct() {
+        $this->role_field = 'role_id';
+        
+
+        
+        $this->perms[0]['server']['graph_add']        = true;
+        $this->perms[0]['server']['edit']             = true;
+    }
+    /**
+     * The main method, determines if the a user is allowed to view a site
+     * @author ChristianGaertner
+     * @return boolean
+     */
+    public function auth()
+    {
+        $CI =& get_instance();
+        
+        if (!isset($CI->session))
+        { # Sessions are not loaded
+            $CI->load->library('session');
+        }
+        
+        if (!isset($CI->session))
+        { # Router is not loaded
+            $CI->load->library('router');
+        }
+        
+        
+        $class = $CI->router->fetch_class();
+        $method = $CI->router->fetch_method();
+
+        // Is rule defined?
+        $is_ruled = false;
+
+        foreach ($this->perms as $role)
+        { # Loop through all rules
+
+            if (isset($role[$class][$method]))
+            { # For this role exists a rule for this route
+                $is_ruled = true;
+            }
+
+        }
+
+        if (!$is_ruled)
+        { # No rule defined for this route
+            // ignording the ACL
+            return;
+        }
+
+        
+        
+        if ($CI->session->userdata($this->role_field))
+        { # Role_ID successfully determined ==> User is logged in
+            if ($this->perms[$CI->session->userdata($this->role_field)][$class][$method])
+            { # The user is allowed to enter the site
+                return true;
+            }
+            else
+            { # The user does not have permissions
+                $CI->error->show(403);
+            }
+        }
+        else
+        { # not logged in
+            $CI->session->set_flashdata('error', $CI->config->item('error_loggedin'));
+            
+            // REDIRECT HERE
+        }
+
+        
+        
+    }
+}
